01832bded71fbe075cb454b365c8d034eff32921
mikkelsen1998 <71692943+mikkelsen1998@users.noreply.github.com>
2020-12-01 12:13:17 +0100

    SQL

--- src/sample/dal/db/DbConnectionProvider.java
+++ src/sample/dal/db/DbConnectionProvider.java
@@ -1,10 +1,12 @@
 package sample.dal.db;
 
 import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Connection;
 import java.util.Properties;
 
 public class DbConnectionProvider {
@@ -13,6 +15,15 @@ public class DbConnectionProvider {
 
     public DbConnectionProvider() throws IOException
     {
+        dataSource = new SQLServerDataSource();
+        dataSource.setServerName("10.176.111.31");
+        dataSource.setDatabaseName("MyTunes3");
+        dataSource.setUser("CSe20A_26");
+        dataSource.setPassword("CSe20A_26");
 
+    }
+    public Connection getConnection() throws SQLServerException;
+    {
+        return dataSource.getConnection();
     }
 }
