eadc062e07f47abd089a8154c02b0de470cbb11d
Mina Anne Christensen <annechristensen88@live.dk>
2020-12-01 12:03:05 +0100

    Update DbConnectionProvider.java

--- src/sample/dal/db/DbConnectionProvider.java
+++ src/sample/dal/db/DbConnectionProvider.java
@@ -1,5 +1,7 @@
 package sample.dal.db;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -7,14 +9,10 @@ import java.util.Properties;
 
 public class DbConnectionProvider {
 
+    private SQLServerDataSource dataSource;
+
     public DbConnectionProvider() throws IOException
     {
 
-        databaseProperties.load(new FileInputStream(new File(PROP_FILE)));
-
-        String server = databaseProperties.getProperty("10.176.111.31");
-        String database = databaseProperties.getProperty("NRS_20");
-        String user = databaseProperties.getProperty("CSe20A_26");
-        String password = databaseProperties.getProperty("CSe20A_26");
     }
 }
