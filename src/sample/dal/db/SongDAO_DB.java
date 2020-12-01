fba1026414a930b0b7056d03f4e266f4343107fb
mikkelsen1998 <71692943+mikkelsen1998@users.noreply.github.com>
2020-12-01 13:54:24 +0100

    hej

--- src/sample/dal/db/SongDAO_DB.java
+++ src/sample/dal/db/SongDAO_DB.java
@@ -4,6 +4,9 @@ import com.microsoft.sqlserver.jdbc.SQLServerException;
 import sample.be.Song;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,8 +17,28 @@ public class SongDAO_DB {
         myDatabaseConnector = new MyDatabaseConnector();
     }
 
-    public List<Song> getAllSong() {
-        List<Song> songs = new ArrayList<>();
+    public List<Song> getAllSong() throws IOException {
+        ArrayList<Song> songs = new ArrayList<>();
+        try (Connection connection = myDatabaseConnector.getConnection()) {
+            String sql = "SELECT * FROM songs";
+
+            Statement statement = connection.createStatement();
+
+            if (statement.execute(sql))
+            {
+                ResultSet resultSet = statement.getResultSet();
+                while (resultSet.next()){
+                    String Title = resultSet.getString("Title");
+                    String Artist = resultSet.getString("Artist");
+                    String Category = resultSet.getString("Category");
+                    String Duration = resultSet.getString("Duration");
+
+                    Song song = new Song(Title, Artist, Category, Duration);
+                    Song.add(song);
+                }
+
+            }
+        }
         
     }
 
