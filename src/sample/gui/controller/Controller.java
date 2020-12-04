c33bfaa740ba9a04ec3b4458cb337873fded41a1
mich163w <mich163w@easv365.dk>
2020-12-04 10:10:17 +0100

    C

--- src/sample/gui/controller/Controller.java
+++ src/sample/gui/controller/Controller.java
@@ -1,16 +1,25 @@
 package sample.gui.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import sample.be.Playlist;
 import sample.be.Song;
 import sample.gui.model.SongModel;
+
+import java.util.List;
 
 public class Controller {
 
     private SongModel songModel;
 
     @FXML
-    private ListView<Song> lstMovies;
+    private ListView<Song> lstSong;
+    @FXML
+    private ListView<Playlist> lstPlaylist;
+    @FXML
+    private Button createSong;
 
     
 
