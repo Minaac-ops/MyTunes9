8a99d2a6e82688cb285b4d5e2a78b5dd7ac0789a
Mina Anne Christensen <annechristensen88@live.dk>
2020-12-08 21:57:42 +0100

    playlistbuttons

---
+++ src/sample/gui/controller/NewPlaylistController.java
@@ -0,0 +1,16 @@
+package sample.gui.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class NewPlaylistController {
+
+
+
+}
